4331f4c21dad298301c147184153aa535fc49803
Juan Brito <juannbritto@gmail.com>
2026-05-31 23:56:31 -0300

    feat: adiciona historico clinico do paciente

---
+++ src/main/java/com/medflow/dto/UpdatePatientHealthRequest.java
@@ -0,0 +1,51 @@
+package com.medflow.dto;
+
+import java.util.List;
+
+public class UpdatePatientHealthRequest {
+    private String recordId;
+    private Integer systolic;
+    private Integer diastolic;
+    private Integer heartRate;
+    private List<String> medications;
+
+    public String getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(String recordId) {
+        this.recordId = recordId;
+    }
+
+    public Integer getSystolic() {
+        return systolic;
+    }
+
+    public void setSystolic(Integer systolic) {
+        this.systolic = systolic;
+    }
+
+    public Integer getDiastolic() {
+        return diastolic;
+    }
+
+    public void setDiastolic(Integer diastolic) {
+        this.diastolic = diastolic;
+    }
+
+    public Integer getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(Integer heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public List<String> getMedications() {
+        return medications;
+    }
+
+    public void setMedications(List<String> medications) {
+        this.medications = medications;
+    }
+}
